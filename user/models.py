42cd2aca183f4a481404f780c0b081c51cf77e77
Batyrzhan <batyrzhantogaev2002@icloud.com>
2025-04-02 21:35:48 +0500

    Created models for User and subcription

--- user/models.py
+++ user/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class SubscriptionUser(models.Model):
+    STATUS_CHOICES = [
+        ('free', 'Free'),
+        ('vip', 'Vip'),
+        ('premium', 'Premium'),
+    ]
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        unique=True)
+
+    def __str__(self):
+        return self.get_status_display()
+
+
+class CustomUser(AbstractUser):
+    photo = models.ImageField(upload_to='photos/', blank=True, null=True)
+    status = models.ForeignKey(
+        SubscriptionUser,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='users'
+        )
